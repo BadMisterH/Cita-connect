317072a0c3a0bb9b88f6879d8f8f0fd9e0b6d892
BadMisterH <60579407+BadMisterH@users.noreply.github.com>
2022-04-16 19:34:47 +0200

    no fonction

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import "./styles/index.scss"
 
 export default function App() {
   return (
-    <View style={Appstyles.container}>
-      <Text>s</Text>
+    <View>
+      <Text>Eren</Text>
       <StatusBar style="auto" />
     </View>
   );
