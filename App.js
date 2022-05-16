2a7c6a124f7de1d58a982f1a7544b007c134c414
BadMisterH <60579407+BadMisterH@users.noreply.github.com>
2022-05-16 13:35:38 +0200

    Add files via upload

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+// import AppliAfficheSignInSignUp from "./pages/stacks/ApplicationAffichages";
+import AuthStackAffichages from './pages/stacks/AuthStackAffichages';
+import { NavigationContainer } from "@react-navigation/native";
+import ApplicationScreen from './pages/stacks/ApplicationAffichages';
 
-export default function App() {
+ const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text></Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+        <ApplicationScreen/>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFFEDC',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
