eedbc93b50da527f4389cf1e1a85b1ae29251fa5
dongjounkim <dongjounkim@gmail.com>
2022-05-22 19:56:44 +0200

    pushed

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
-import 'react-native-gesture-handler';
-import * as React from 'react';
-import AuthNav from "./AuthNav";
+import { View, Text, Image } from 'react-native';
+import React from 'react';
+import Appstyles from './styles/styles.scss';
 
-// import AppliAfficheSignInSignUp from "./pages/stacks/ApplicationAffichages";
-// import AuthStackAffichages from './pages/stacks/AuthStackAffichages';
-import { NavigationContainer } from "@react-navigation/native";
-import ApplicationScreen from './pages/stacks/ApplicationAffichages';
-
- const App = () => {
-
+export default function App() {
   return (
-    <NavigationContainer>
-        <ApplicationScreen/>
-    </NavigationContainer>
-  );
-};
-
-export default App;
+    <View style={Appstyles.logoContainer}>
+      <Text>App</Text>
+      <Image
+          style={Appstyles.tinyLogo}
+          source={require('./assets/img/favicon.png')}
+      />
+    </View>
+  )
+}
